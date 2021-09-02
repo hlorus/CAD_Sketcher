0b9a78cf0bff6767cb846878db5da75c3250c9be
David Friedli <david@friedli-be.ch>
2021-09-02 15:25:19 +0200

    Gizmos: Avoid drawing generic gizmo for constraints that already have a specific one

--- gizmos.py
+++ gizmos.py
@@ -312,6 +312,19 @@ class VIEW3D_GGT_slvs_preselection(GizmoGroup):
     def setup(self, context):
         self.gizmo = self.gizmos.new(VIEW3D_GT_slvs_preselection.bl_idname)
 
+specific_constraint_types = ("angle", "diameter", "distance")
+
+def generic_constraints(context):
+    """ Iterate through constraints which don't have a specific gizmo """
+    constrs = context.scene.sketcher.constraints
+    for prop_list in constrs.rna_type.properties:
+        name = prop_list.identifier
+        if name in ("name", "rna_type", *specific_constraint_types):
+            continue
+        list = getattr(constrs, name)
+
+        for entity in list:
+            yield entity
 
 # TODO: This could alrady Skip entities and constraints that are not active
 # TODO: only store indices instead of actual objects
@@ -319,7 +332,7 @@ def constraints_mapping(context):
     # Get a constraints per entity mapping
     entities = []
     constraints = []
-    for c in context.scene.sketcher.constraints.all:
+    for c in generic_constraints(context):
         for e in c.entities():
             if e not in entities:
                 entities.append(e)
