26e31651d86c3febe12a428b4372b0bcd7f25b3a
hlorus <64740362+hlorus@users.noreply.github.com>
2022-05-02 09:42:05 +0200

    Bump version to 0.22.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "CAD Sketcher",
     "author": "hlorus",
-    "version": (0, 22, 0),
+    "version": (0, 22, 1),
     "blender": (2, 80, 0),
     "location": "View3D > Toolbar",
     "description": "Parametric, constraint-based geometry sketcher",
