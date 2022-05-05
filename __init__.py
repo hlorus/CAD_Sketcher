04bd22cc54b412c997f2d71917de1f7d584c0d2e
TheMakerTales <103122109+TheMakerTales@users.noreply.github.com>
2022-05-05 10:03:19 +0100

    Gumroad Version Bump

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "CAD Sketcher",
     "author": "hlorus",
-    "version": (0, 22, 2),
+    "version": (0, 22, 3),
     "blender": (2, 80, 0),
     "location": "View3D > Toolbar",
     "description": "Parametric, constraint-based geometry sketcher",
