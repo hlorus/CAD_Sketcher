2b756e9a5403945ecaa332e8c8cb61acfa6a5d59
David Friedli <david@friedli-be.ch>
2022-06-04 12:22:15 +0200

    Ui: Display coonstraint dependencies in context menu with show_debug_settings option

--- class_defines.py
+++ class_defines.py
@@ -1982,6 +1982,16 @@ class GenericConstraint:
 
         return c
 
+    def draw_props(self, layout):
+        if not preferences.is_experimental():
+            return
+
+        col = layout.column()
+        col.label(text="Dependencies")
+        for e in self.dependencies():
+            col = layout.column()
+            col.label(text=str(e))
+
 
 # NOTE: When tweaking it's necessary to constrain a point that is only temporary available
 # and has no SlvsPoint representation
@@ -2320,6 +2330,7 @@ class SlvsDistance(GenericConstraint, PropertyGroup):
         self.draw_offset = pos[1] / ui_scale
 
     def draw_props(self, layout):
+        super().draw_props(layout)
         layout.prop(self, "value")
         layout.separator()
 
@@ -2390,6 +2401,7 @@ class SlvsDiameter(GenericConstraint, PropertyGroup):
         self.draw_offset = pos.length
 
     def draw_props(self, layout):
+        super().draw_props(layout)
         layout.prop(self, "value")
 
     def value_placement(self, context):
@@ -2527,6 +2539,7 @@ class SlvsAngle(GenericConstraint, PropertyGroup):
         self.draw_offset = math.copysign(pos.length / ui_scale, pos.x)
 
     def draw_props(self, layout):
+        super().draw_props(layout)
         layout.prop(self, "value")
         layout.prop(self, "setting")
 
@@ -2837,6 +2850,7 @@ class SlvsRatio(GenericConstraint, PropertyGroup):
         return value, None
 
     def draw_props(self, layout):
+        super().draw_props(layout)
         layout.prop(self, "value")
 
 
