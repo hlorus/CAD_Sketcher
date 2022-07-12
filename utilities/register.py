b86b75489d1df4a073c05706f3efa3cad5df7bba
David Friedli <david@friedli-be.ch>
2022-07-12 18:54:40 +0200

    Allow hot-reload by cleaning sys.modules
    Instead of using importlib.reload

---
+++ utilities/register.py
@@ -0,0 +1,9 @@
+import sys
+
+# From: https://github.com/iyadahmed/bpy_helper/blob/main/bpy_helper/register.py
+def cleanse_modules(parent_module_name):
+    """search for your plugin modules in blender python sys.modules and remove them"""
+
+    for module_name in list(sys.modules.keys()):
+        if module_name.startswith(parent_module_name):
+            del sys.modules[module_name]
