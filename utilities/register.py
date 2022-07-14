fcb2a57a2f8e6cacba4f28106c1e21277102998d
David Friedli <david@friedli-be.ch>
2022-07-14 16:16:52 +0200

    Fix logger naming

--- utilities/register.py
+++ utilities/register.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 # From: https://github.com/iyadahmed/bpy_helper/blob/main/bpy_helper/register.py
@@ -7,3 +8,11 @@ def cleanse_modules(parent_module_name):
     for module_name in list(sys.modules.keys()):
         if module_name.startswith(parent_module_name):
             del sys.modules[module_name]
+
+def get_path():
+    return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+def get_name():
+    return os.path.basename(get_path())
+
