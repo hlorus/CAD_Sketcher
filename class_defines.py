639f0e8a8a0e5ffb9608810f93b6a69223b12a9d
maciejgornicz <maciej.gornicz@gmail.com>
2022-08-08 17:19:12 +0200

    some code cleanup and logging

--- class_defines.py
+++ class_defines.py
@@ -297,6 +297,9 @@ class SlvsGenericEntity:
             if ob.sketch_index == index_old:
                 ob.sketch_index = index_new
 
+    def connection_points(self):
+        return []
+
     def dependencies(self):
         return []
 
