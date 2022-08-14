f3567e641302e93fb16e6de72bc935b04f04e973
laundmo <laurinschmidt2001@gmail.com>
2022-08-14 21:13:06 +0200

    comments and documentation

--- utilities/install.py
+++ utilities/install.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 def check_module(package):
-    # Note: Blender might be installed in a directory that needs admin rights and thus defaulting to a user installation.
-    # That path however might not be in sys.path....
     refresh_path()
+    # This was done in the past to make sure modules in usersite can be found.
+    # Since using USERBASE this shouldnt be necessary, but it's left as a fallback.
     p = site.USER_SITE
     if p not in sys.path:
         sys.path.insert(0, p)
@@ -24,10 +24,13 @@ def check_module(package):
         raise e
 
 
-def install_pip():
+def install_pip() -> int:
     import ensurepip
 
+    # ensurepip.bootstrap just hides the return
+    # code which is needed for the currect error messages.
     ret_val = ensurepip._bootstrap()
+    # Env var left over from pip installation, needs to be removed
     os.environ.pop("PIP_REQ_TRACKER", None)
     return ret_val
 
@@ -39,7 +42,7 @@ def check_pip_installed() -> bool:
     return False
 
 
-def ensure_pip():
+def ensure_pip() -> bool:
     refresh_path()
     if not check_pip_installed():
         return install_pip() == 0
@@ -51,16 +54,18 @@ def refresh_path():
 
     # set the user base directory
     os.environ["PYTHONUSERBASE"] = global_data.CUSTOM_USER_BASE
+    # Applies the PYTHONUSERBASE env var
     importlib.reload(site)
 
 
-def install_package(package: str, no_deps: bool = True):
+def install_package(package: str, no_deps: bool = True) -> bool:
     refresh_path()
-    _install_package("pip")  # update pip
+    _install_package("pip")  # update pip if needed
     return _install_package(package, no_deps)
 
 
-def _install_package(package: str, no_deps: bool = True):
+def _install_package(package: str, no_deps: bool = True) -> bool:
+    # --user is needed to ensure install always happens into USERBASE
     cmd = [global_data.PYPATH, "-m", "pip", "install", "--upgrade", "--user"]
 
     if no_deps:
