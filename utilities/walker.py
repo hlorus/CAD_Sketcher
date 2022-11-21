2cba8537599b482d8ba385ac9042471dffebc555
David Friedli <david@friedli-be.ch>
2022-11-21 08:05:31 +0100

    Cleanup: Split EntityWalker out of BezierConverter

---
+++ utilities/walker.py
@@ -0,0 +1,156 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def point_entity_mapping(scene):
+    """Get a entities per point mapping"""
+
+    points = []
+    entities = []
+    for entity in scene.sketcher.entities.all:
+        if entity.is_point():
+            continue
+        if not hasattr(entity, "connection_points"):
+            continue
+        for p in entity.connection_points():
+            if not p.is_point():
+                continue
+            if p not in points:
+                points.append(p)
+            i = points.index(p)
+            if i >= len(entities):
+                entities.append([])
+            ents = entities[i]
+            if entity not in ents:
+                ents.append(entity)
+    assert len(points) == len(entities)
+    return points, entities
+
+
+def shares_point(seg_1, seg_2):
+    points = seg_1.connection_points()
+    for p in seg_2.connection_points():
+        if p in points:
+            return True
+    return False
+
+
+class EntityWalker:
+    """Utility class to find connected entities"""
+
+    def __init__(self, scene, sketch):
+        self.sketch_entities = []
+        self.paths = []
+        self.ok = True
+        self.scene = scene
+        self.sketch = sketch
+        self.points, self.entities = point_entity_mapping(scene)
+
+        # TODO: use sketch.entities?
+        sketch_index = self.sketch.slvs_index
+        for e in self.scene.sketcher.entities.all:
+            if not hasattr(e, "sketch") or e.sketch_i != sketch_index:
+                continue
+            if not e.is_path():
+                continue
+            if e.construction:
+                continue
+            self.sketch_entities.append(e)
+
+    @staticmethod
+    def is_cyclic_path(path):
+        if len(path) == 1:
+            return path[0].is_closed()
+
+        first, last = path[0], path[-1]
+
+        # NOTE: first and last segment might be connected on one side only when there are 2 segments
+        if len(path) == 2:
+            return all(
+                item in first.connection_points() for item in last.connection_points()
+            )
+        elif shares_point(first, last):
+            return True
+        return False
+
+    def _get_connected_entities(self, point):
+        return self.entities[self.points.index(point)]
+
+    def _branch_path(self):
+        self.paths.append(([], []))
+        return self.paths[-1]
+
+    # TODO: rename ignore_point -> start_point / rename path -> spline_path
+    def walker(self, entity, path, ignore_point=None, invert=False):
+        segments = path[0]
+        logger.debug(
+            "goto: {} entrypoint: {} invert_walker {}".format(
+                entity, ignore_point, invert
+            )
+        )
+
+        if invert:
+            # NOTE: this might be slow
+            segments.insert(0, entity)
+        else:
+            segments.append(entity)
+
+        self.sketch_entities.remove(entity)
+
+        # Not great..
+        if entity.is_closed():
+            path[1].append(False)
+
+        # NOTE: check through connecting points of entity
+        # should respect all points that lie on actual geometry,
+        # should also include coincident constraints
+
+        points = list(
+            filter(
+                (lambda p: p.is_point() and p != ignore_point),
+                entity.connection_points(),
+            )
+        )
+        entities = []
+        for point in points:
+            ents = self._get_connected_entities(point)
+            ents = filter((lambda e: e != entity and e in self.sketch_entities), ents)
+            entities.append(ents)
+
+        # NOTE: this should also invert and also happen if we can follow a segment from that point
+        # point = next(points, None)
+        point = points[0] if len(points) else None
+        if point:
+            invert_direction = entity.direction(
+                (ignore_point if ignore_point else point),
+                is_endpoint=(not ignore_point),
+            )
+            if invert:
+                invert_direction = not invert_direction
+                path[1].insert(0, invert_direction)
+            else:
+                path[1].append(invert_direction)
+
+        for point, ents in zip(points, entities):
+            # check through connected entities
+            branch = False
+            for e in ents:
+                if branch:
+                    path = self._branch_path()
+                branch = True
+
+                self.walker(e, path, ignore_point=point, invert=invert)
+
+            # TODO: path could also split here...
+
+            # Invert walker when there's a second connection point
+            invert = not invert
+
+    def run(self):
+        while len(self.sketch_entities):
+            start_entity = self.sketch_entities[0]
+            logger.info("Start path walker at {}".format(start_entity))
+            self.walker(start_entity, self._branch_path())
+
+        # TODO: check path, set self.ok
