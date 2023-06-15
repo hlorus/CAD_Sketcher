0963cc45b80e949cb09e537d1e6f96bb1e4f2435
David Friedli <david@friedli-be.ch>
2023-06-15 08:27:02 +0200

    Ops: Improve asset loading
    - Load from init method
    - Cancel operator if asset cannot be loaded
    - Set Fake User
    - Look through all blend files in asset library

--- operators/modifiers.py
+++ operators/modifiers.py
@@ -1,3 +1,5 @@
+import glob
+
 import bpy
 from bpy.types import Operator, Object, Context
 from bpy.props import FloatProperty
@@ -8,6 +10,31 @@ from ..declarations import Operators
 from ..utilities.view import get_placement_pos
 from ..stateful_operator.utilities.register import register_stateops_factory
 from ..stateful_operator.state import state_from_args
+
+
+def load_asset(library, asset_type, asset):
+    """Loads an asset of given type from a specified library
+    Returns True if it is loaded or already present in file"""
+
+    # Check if the asset is already present in file
+    if asset in [a.name for a in getattr(bpy.data, asset_type)]:
+        return True
+
+    prefs = bpy.context.preferences
+    fp = prefs.filepaths.asset_libraries[library].path
+
+    for file in glob.glob(fp + "/*.blend"):
+        with bpy.data.libraries.load(file, assets_only=True) as (data_from, data_to):
+            coll = getattr(data_from, asset_type)
+            if not asset in coll:
+                continue
+            getattr(data_to, asset_type).append(asset)
+
+        group = bpy.data.node_groups.get("Extrude")
+        group.use_fake_user = True
+
+        return True
+    return False
 
 
 class View3D_OT_node_extrude(Operator, Operator3d):
@@ -25,7 +52,7 @@ class View3D_OT_node_extrude(Operator, Operator3d):
             description="Profile to extrude",
             pointer="object",
             types=(Object,),
-            use_create=False,  # Should be the default, No?
+            use_create=False,
         ),
         state_from_args(
             "Offset",
@@ -36,6 +63,8 @@ class View3D_OT_node_extrude(Operator, Operator3d):
         ),
     )
 
+    NODEGROUP_NAME = "Extrude"
+
     def get_offset(self, context: Context, coords):
         pos = get_placement_pos(context, coords)
         if pos is None:
@@ -45,30 +74,28 @@ class View3D_OT_node_extrude(Operator, Operator3d):
         delta = (mat @ Vector(pos)).z
         return delta
 
+    # Somhow doesn't seem to work, might be an undo problem
+    def init(self, context, event):
+        if not load_asset("resources", "node_groups", "Extrude"):
+            self.report({"ERROR"}, f"Cannot load asset \"{self.NODEGROUP_NAME}\" from library")
+            return False
+
+        bpy.ops.ed.undo_push(message=f"Load Asset \"{self.NODEGROUP_NAME}\"")
+        return True
+
     def main(self, context):
         ob = self.object.original
         offset = self.offset
-
+        
         # Add a modifier to object
-        mod = ob.modifiers.new("CADSketcher Extrude", "NODES")
-
-        # Get node group from asset library
-        path_resources = (
-            context.preferences.filepaths.asset_libraries["resources"].path
-            + "/assets.blend"
-        )
-        with bpy.data.libraries.load(str(path_resources), assets_only=True) as (
-            data_from,
-            data_to,
-        ):
-            data_to.node_groups.append("Extrude")
+        modifier = ob.modifiers.new("CAD_Sketcher Extrude", "NODES")
 
         # Add nodegroup to modifier
-        group = bpy.data.node_groups.get("Extrude")
-        mod.node_group = group
+        nodegroup = bpy.data.node_groups.get(self.NODEGROUP_NAME)
+        modifier.node_group = nodegroup
 
         # Set offset
-        mod["Input_2"] = offset
+        modifier["Input_2"] = offset
         ob.update_tag()
 
         return True
