e574579bf0b28f54fc62d47be95d5f2f4ad65115
David Friedli <david@friedli-be.ch>
2024-03-21 08:52:25 +0100

    Fix versionnumber lookup

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,7 +3,7 @@ import pathlib, toml
 def get_addon_version():
     """Return addon version from manifest file"""
 
-    manifest = pathlib.Path(__file__).parent / "blender_manifest.toml"
+    manifest = pathlib.Path(__file__).parent.parent / "blender_manifest.toml"
     try:
         return toml.load(manifest)["version"]
     except Exception:
