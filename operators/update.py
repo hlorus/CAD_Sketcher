051dd32bb9eb45e44cac023346cbb9def7b5ba9f
ra100 <git@ra100.net>
2025-06-22 13:25:25 +0200

    Fix Update action in sketch

--- operators/update.py
+++ operators/update.py
@@ -1,8 +1,7 @@
-import bpy
 from bpy.types import Operator
 from bpy.utils import register_classes_factory
 
-from ..solver import Solver
+from ..solver import solve_system
 from ..converters import update_convertor_geometry
 
 
@@ -11,9 +10,8 @@ class VIEW3D_OT_update(Operator):
     bl_label = "Update"
 
     def execute(self, context):
-        update_convertor_geometry()
-        solvesys = Solver()
-        solvesys.solve()
+        update_convertor_geometry(context.scene)
+        solve_system(context)
         return {'FINISHED'}
 
 
