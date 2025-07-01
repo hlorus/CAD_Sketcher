3fa4589f64b9acb3125e9456a4037ae43b94fee7
David Friedli <david@friedli-be.ch>
2025-07-01 09:06:45 +0200

    Revert absolute path

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-/home/yoga/data/blender-git/build_linux_lite/bin/blender --python ./testing/__init__.py -- --log_level=INFO
+blender --addons CAD_Sketcher --python ./testing/__init__.py -- --log_level=INFO
