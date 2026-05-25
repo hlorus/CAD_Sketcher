8f62d6d29b004a6cb9814947857d3104a4ecb74c
falken10vdl <falken10vdl@gmail.com>
2026-05-25 13:00:39 +0200

    Enhance Point2D class with selection detection and color indication for selected line origins

--- model/point_2d.py
+++ model/point_2d.py
@@ -3,16 +3,18 @@ import logging
 from typing import List
 
 import bpy
-from bpy.types import PropertyGroup
+from bpy.types import PropertyGroup, Context
 from bpy.props import FloatVectorProperty
 from gpu_extras.batch import batch_for_shader
 from mathutils import Matrix, Vector
 from bpy.utils import register_classes_factory
 
 from ..utilities.draw import draw_rect_2d
+from ..declarations import WorkSpaceTools
 from ..solver import Solver
 from .base_entity import SlvsGenericEntity, Entity2D, tag_update
 from .utilities import slvs_entity_pointer, make_coincident
+from .line_2d import SlvsLine2D
 from ..utilities.constants import HALF_TURN
 
 logger = logging.getLogger(__name__)
@@ -22,6 +24,30 @@ class Point2D(Entity2D):
     @classmethod
     def is_point(cls):
         return True
+
+    def _is_selected_line_origin(self, context: Context) -> bool:
+        """True when this point is p1 of a selected line in select tool mode."""
+        workspace = getattr(context, "workspace", None)
+        if workspace is None:
+            return False
+
+        tool = workspace.tools.from_space_view3d_mode(context.mode)
+        if tool is None or tool.idname != WorkSpaceTools.Select:
+            return False
+
+        sketch = context.scene.sketcher.active_sketch
+        if sketch is None:
+            return False
+
+        sse = context.scene.sketcher.entities
+        for entity in sse.selected_active:
+            if not isinstance(entity, SlvsLine2D):
+                continue
+            if entity.sketch != sketch:
+                continue
+            if entity.p1.slvs_index == self.slvs_index:
+                return True
+        return False
 
     def update(self):
         if bpy.app.background:
@@ -63,6 +89,17 @@ class Point2D(Entity2D):
     def closest_picking_point(self, origin, view_vector):
         """Returns the point on this entity which is closest to the picking ray"""
         return self.location
+
+    @property
+    def point_size(self):
+        if self._is_selected_line_origin(bpy.context):
+            return 1.1 * super().point_size
+        return super().point_size
+
+    def color(self, context: Context):
+        if self._is_selected_line_origin(context):
+            return (1.0, 0.0, 0.0, 1.0)
+        return super().color(context)
 
 
 class SlvsPoint2D(Point2D, PropertyGroup):
