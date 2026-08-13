3f3b04b9a3216709a4c9050790dacd3436aa6927
franklincg <64386813+franklincg@users.noreply.github.com>
2026-08-12 22:08:50 -0300

    Declare projection operator

--- declarations.py
+++ declarations.py
@@ -69,6 +69,7 @@ class Operators(str, Enum):
     NodeExtrude = "view3d.slvs_node_extrude"
     NodeArrayLinear = "view3d.slvs_node_array_linear"
     NodeRevolve = "view3d.slvs_node_revolve"
+    ProjectGeometry = "view3d.slvs_project_geometry"
     RegisterDrawCB = "view3d.slvs_register_draw_cb"
     RenameCurve = "view3d.slvs_rename_curve"
     Restore = "view3d.slvs_restore"
