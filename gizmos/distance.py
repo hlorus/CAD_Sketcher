3faef01d16d2f2f5757265502ffb09d36e48402e
franklincg <64386813+franklincg@users.noreply.github.com>
2026-08-12 21:50:16 -0300

    Draw distance gizmos for 3D entities

--- gizmos/distance.py
+++ gizmos/distance.py
@@ -2,13 +2,20 @@ import math
 
 from bpy.types import Gizmo, GizmoGroup
 from mathutils import Vector
-from mathutils.geometry import intersect_point_line
+from mathutils.geometry import distance_point_to_plane, intersect_point_line
 
 from ..declarations import GizmoGroups, Gizmos
 from ..model.types import SlvsDistance
+from ..model.workplane import SlvsWorkplane
 from ..utilities.view import get_scale_from_pos
 from .base import ConstraintGenericGGT, ConstraintGizmoGeneric
 from .utilities import draw_arrow_shape, get_arrow_size, get_overshoot
+
+
+def _position(entity):
+    if hasattr(entity, "co"):
+        return Vector(entity.co)
+    return Vector(entity.location)
 
 
 class VIEW3D_GGT_slvs_distance(GizmoGroup, ConstraintGenericGGT):
@@ -39,18 +46,24 @@ class VIEW3D_GT_slvs_distance(Gizmo, ConstraintGizmoGeneric):
         ui_scale = context.preferences.system.ui_scale
         dist = constr.value / 2 / ui_scale
         offset = self.target_get_value("offset")
-        entity1, entity2 = constr.ref(1), constr.ref(2)
-        if entity1 is None or entity2 is None:
-            return ((0,0,0),) * 4
+        entity1 = constr._resolved_entity(1)
+        entity2 = constr._resolved_entity(2)
+        if entity1 is None:
+            return ((0, 0, 0),) * 4
         if entity1.is_line():
             entity1, entity2 = entity1.p1, entity1.p2
+        if entity2 is None:
+            return ((0, 0, 0),) * 4
 
         # Get constraints points in local space and adjust helplines
         # based on their position
         mat_inv = constr.matrix_basis().inverted()
 
         def get_local(point):
-            return (mat_inv @ point.to_3d()) / ui_scale
+            point = Vector(point)
+            if len(point) == 2:
+                point = point.to_3d()
+            return (mat_inv @ point) / ui_scale
 
         # Store the two endpoints of the helplines in local space
         points_local = []
@@ -66,25 +79,26 @@ class VIEW3D_GT_slvs_distance(Gizmo, ConstraintGizmoGeneric):
                     centerpoint, entity2.p1.co, entity2.p2.co
                 )
             else:
-                # TODO: Handle the case for SlvsWorkplane
-                pass
+                return ((0, 0, 0),) * 4
 
             targetvec = targetpoint - centerpoint
-            points_local.append(get_local(
-                centerpoint + entity1.radius * targetvec / targetvec.length
-            ))
+            if targetvec.length == 0:
+                return ((0, 0, 0),) * 4
+            points_local.append(
+                get_local(centerpoint + entity1.radius * targetvec / targetvec.length)
+            )
 
         else:
-            points_local.append(get_local(entity1.location))
+            points_local.append(get_local(_position(entity1)))
 
         # Add endpoint for entity2 helpline
         if entity2.is_point():
-            points_local.append(get_local(entity2.location))
+            points_local.append(get_local(_position(entity2)))
 
         elif entity2.is_line():
             line_points = (
-                get_local(entity2.p1.location),
-                get_local(entity2.p2.location),
+                get_local(_position(entity2.p1)),
+                get_local(_position(entity2.p2)),
             )
             line_points_side = [pos.y - offset > 0 for pos in line_points]
 
@@ -101,9 +115,17 @@ class VIEW3D_GT_slvs_distance(Gizmo, ConstraintGizmoGeneric):
                 y = line_points[i].y
             points_local.append(Vector((x, y, 0.0)))
 
+        elif isinstance(entity2, SlvsWorkplane):
+            p1 = _position(entity1)
+            normal = Vector(entity2.normal).normalized()
+            signed_distance = distance_point_to_plane(
+                p1, entity2.p1.location, normal
+            )
+            points_local.append(get_local(p1 - normal * signed_distance))
+
         # Pick the points based on their x location
         if len(points_local) < 2:
-            return ((0,0,0),) * 4
+            return ((0, 0, 0),) * 4
         if points_local[0].x > points_local[1].x:
             point_right, point_left = points_local
         else:
@@ -166,9 +188,7 @@ class VIEW3D_GT_slvs_distance(Gizmo, ConstraintGizmoGeneric):
                 # jitter back and forth to extend leader line for
                 # text_outside case but it is unnecessary work for
                 # text_inside case
-                Vector(
-                    (outset, offset, 0)
-                ),
+                Vector((outset, offset, 0)),
                 p1,
                 p2,
                 *draw_arrow_shape(
